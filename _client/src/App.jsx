487e55c800e0dfaa923110434dde9ce0eaa9f29f
adarshsharma1350 <111039432+adarshsharma1350@users.noreply.github.com>
2024-03-02 01:05:00 +0530

    its all the starting

---
+++ _client/src/App.jsx
@@ -0,0 +1,27 @@
+// import React from "react";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from "./Components/Header";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
