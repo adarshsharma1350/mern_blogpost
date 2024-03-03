83b232f5936cb58fd1b7643ff49b2324a165d21d
adarshsharma1350 <111039432+adarshsharma1350@users.noreply.github.com>
2024-03-03 11:48:43 +0530

    just commited

--- _client/src/redux/store.js
+++ _client/src/redux/store.js
@@ -1,9 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import userReducer from './user/userSlice';
-// import { useReducer } from 'react';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+// import  from 'redux-persist';
+
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 
 export const store = configureStore({
-  reducer: {
-    user:userReducer,
-  },
-})
+  reducer: persistReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({serializableCheck: false}),
+
+});
+
+export const persistor = persistStore(store);
